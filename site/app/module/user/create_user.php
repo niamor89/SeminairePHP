8cbdba21ceb2984b787a4449e21f58498db24d64
Antoine GALTIER <antoine.galtier@gmail.com>
2013-03-08 14:50:30 +0100

    Correction Inscription

--- site/app/module/user/create_user.php
+++ site/app/module/user/create_user.php
@@ -2,12 +2,12 @@
 $context["title"] = "Création de compte";
 $nb = add_user($_POST['pseudo'], 
 			 md5($_POST['pass']), 
-			(isset($_POST['prenom']))? $_POST['prenom'] : "", 
-			(isset($_POST['nom']))? $_POST['nom'] : "", 
+			$_POST['prenom'], 
+			$_POST['nom'], 
 			 $_POST['mail'], 
-			(isset($_POST['phone']))? $_POST['phone'] : "", 
-			(isset($_POST['adress']))? $_POST['adress'] : "", 
-			(isset($_POST['city']))? $_POST['city'] : "", 
+			((!empty($_POST['phone']))? $_POST['phone'] : "0000000000"), 
+			$_POST['adress'], 
+			$_POST['city'], 
 			 $_POST['birthday']);
 			
 
