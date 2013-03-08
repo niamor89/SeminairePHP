d5369037b005d154f6447ee977edee29c1ce482f
farah <farah.villard@live.com>
2013-03-08 09:12:50 +0100

    widget friends

--- site/app/view/friends/add.php
+++ site/app/view/friends/add.php
@@ -1,2 +1,51 @@
-<?php 
+<script language="Javascript">
+	function visibilite(thingId)
+	{
+		var targetElement;
+		targetElement = document.getElementById(thingId) ;
+		if (targetElement.style.display == "none")
+		{
+			targetElement.style.display = "" ;
+		} 
+		else 
+		{
+			targetElement.style.display = "none" ;
+		}
+	}
+	
+	function okrequest()
+	{
+		resultat = alert('A request was sent');
+		if(resultat =="1")
+		{ 
+			//window.history.go(); 
+		}
+	} 
+</script>
+
+<?php global $tab;
+$tab = farah_register_function();
 ?>
+
+<div id="addfriend">
+	<div id="searchbar">
+			<form method="POST" action="add" name="monform">
+			<input type="text" name="search" id="searching"/>
+			<button>Search</button>
+	</div>
+	<div id="searchfriend">
+		<table border=1>
+			<?php 
+				foreach($tab as $joueur)
+				{?> 
+				<tr> 
+					<td class="farahavatar"> A </td>
+					<td> <div id ="joueur" class ="findPseudo" ><?php echo $joueur["pseudo_Joueur"]; ?></div></td>
+					<td><div class="addbutton" onclick="okrequest(); javascript:visibilite('joueur'); return true;"><button>Add Friend</button></div></td>
+				</tr>
+				<?php
+				}
+				?>
+		</table>
+	</div>
+</div>
--- site/app/view/friends/display.php
+++ site/app/view/friends/display.php
@@ -1,12 +1,55 @@
+<script language="Javascript">	
+function visibilite(thingId)
+	{
+		var targetElement;
+		targetElement = document.getElementById(thingId) ;
+		if (targetElement.style.display == "none")
+		{
+			targetElement.style.display = "" ;
+		} 
+		else 
+		{
+			targetElement.style.display = "none" ;
+		}
+	}
+		
+	function okremove() 
+	{
+		resultat=window.confirm('Are you sure to delete this friend ?');
+		if(resultat =="1")
+		{
+			window.history.go()
+		}
+	} 
+</script>
+
+<div id="friendlist"><label> Friends list </label></div>
+
 <?php 
+
+	global $tab;
+	$tab = farah_register_function();
 	
-	/*$friendtab = array();
-	$i =0;	
-	
-	$friendtab[0] = "Farah";
-	$friendtab[1] = "Romain N";
-	$friendtab[2] = "Antoine";
-	$friendtab[3] = "Ruslan";
-	$friendtab[4] = "Romain T";
-	$friendtab[5] = "Oussmane";*/
+	if ( empty($tab) )
+			{
+				echo '<div><label> Your friend list is empty </label></div>';
+			}
+	else
+	{
 ?>
+			
+<div id="displayfriend">
+	<table border=1>
+		<?php
+			foreach($tab as $joueur)
+			{?>
+			<tr> 
+				<td class="farahavatar"> A </td>
+				<td> <div class ="displaypseudo"></div><?php echo $joueur["pseudo_Joueur"]; ?> </td>
+				<td><div class="removebutton"><?php $removebutton = "Remove friend";?>
+				<button href="display.php" onclick="okremove()"> <?php echo $removebutton;?></button></div></td>
+			</tr>
+		<?php } ?>			
+	</table>
+</div>
+<?php }?>
