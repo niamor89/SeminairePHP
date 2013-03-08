3ebd589964fd39269ff56a611c011c836b581f55
farah <farah.villard@live.com>
2013-03-08 12:58:21 +0100

    add.php

--- site/app/view/friends/add.php
+++ site/app/view/friends/add.php
@@ -16,11 +16,14 @@
 	function okrequest()
 	{
 		resultat = alert('A request was sent');
-		if(resultat =="1")
-		{ 
-			//window.history.go(); 
-		}
-	} 
+		window.stop();  
+	}
+	
+	
+	function reloadpage()
+	{	
+		window.location.reload();
+	}
 </script>
 
 <?php global $tab;
@@ -29,23 +32,42 @@ $tab = farah_register_function();
 
 <div id="addfriend">
 	<div id="searchbar">
-			<form method="POST" action="add" name="monform">
-			<input type="text" name="search" id="searching"/>
+			<form method="POST" action="add">
+			<input type="text" value="<?php $bar="Friend pseudo"?>" name="search" id="searching"/>
 			<button>Search</button>
+			
 	</div>
 	<div id="searchfriend">
-		<table border=1>
-			<?php 
-				foreach($tab as $joueur)
-				{?> 
-				<tr> 
-					<td class="farahavatar"> A </td>
-					<td> <div id ="joueur" class ="findPseudo" ><?php echo $joueur["pseudo_Joueur"]; ?></div></td>
-					<td><div class="addbutton" onclick="okrequest(); javascript:visibilite('joueur'); return true;"><button>Add Friend</button></div></td>
-				</tr>
-				<?php
+			<?php
+				global $tmpplayer;
+
+				if (isset($_POST["search"]))
+				{
+					$searchplayer = $_POST["search"];
+
+					foreach($tab as $joueur)
+					{
+						foreach($joueur as $k=>$v)
+						
+						if ($searchplayer == $v)
+						{ 
+						 
+							if ( $tmpplayer != $searchplayer )
+							{ 
+								$tmpplayer = $searchplayer; ?>				
+								<table border=1>
+								<tr> 
+									<td class="farahavatar"> A </td>
+									<td> <div id ="joueur" class ="findPseudo" ><?php echo $searchplayer; ?></div></td>
+									<td><div class="addbutton" onclick="okrequest();"><button>Add Friend</button></div></td>
+								</tr>
+								</table>
+							<?php
+							}
+						}
+							
+					}
 				}
 				?>
-		</table>
 	</div>
 </div>
