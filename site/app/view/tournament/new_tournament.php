f139aab698d7936622163e474ffc35c7500913b3
farah <farah.villard@live.com>
2013-03-08 12:21:03 +0100

    new_tournament.php

--- site/app/view/tournament/new_tournament.php
+++ site/app/view/tournament/new_tournament.php
@@ -26,10 +26,19 @@ function testcheck(el)
 		return true;
 	}	
 }
+function submitjointournament()
+{
+	window.location.replace("../tournament/join_tournament");
+}
 
-function submitplayers()
+function submitgame()
 {	
-	window.location.replace("../tournament/validate_team");
+	window.location.replace("../session/start");
+}
+
+function submitcreatetournament()
+{	
+	window.location.replace("../tournament/create_tournament");
 }
 </script>
 
@@ -37,16 +46,18 @@ function submitplayers()
 	<table border=1>
 		<?php
 			foreach($newtab as $valeur)
-			{
+			{ 
 			 ?> <tr> 
 				<td class="farahavatar"> A </td>
 				<td> <div class ="queuepseudo"> <?php echo $valeur["pseudo_Joueur"]; ?> </div></td>
 				<td><div class="farahcheckbox">
-				<input type="checkbox"  onClick="return testcheck(this);"></div></td>
+				<input type="checkbox" id="<?php $i; ?>" onClick="return testcheck(this);"></div></td>
 				</tr>
 			<?php
 			}
 			?>
-			<input type="submit" value="Submit" onClick="submitplayers();">
+			<input type="submit" value="Game" onClick="submitgame();">
+			<input type="submit" method="POST" value="Create Tournament" onClick="submitcreatetournament();">
+			<input type="submit" method="POST" value="Join Tournament" onClick="submitjointournament();">
 	</table>
 </div>
