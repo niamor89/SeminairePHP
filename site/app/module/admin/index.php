5494c1f683d16657755619727fc129ec6b05784d
TENAILLE <romain@tenaille.fr>
2013-03-08 16:04:16 +0100

    Admin

--- site/app/module/admin/index.php
+++ site/app/module/admin/index.php
@@ -16,3 +16,12 @@
 	else if(isset($_GET['liste_s'])) {
 		echo json_encode(GetArray('SELECT * FROM t_serveur'));
 	}
+	else if(isset($_GET['liste_p'])) {
+		echo json_encode(GetArray('SELECT * FROM t_produit'));
+	}
+	else if(isset($_GET['liste_sc'])) {
+		echo json_encode(GetArray('select sc.id_achat_sc, sc.date_achat_sc, sc.montant_sc, sc.montant_euros, sc.code_validation, j.pseudo_joueur from t_achat_sc as sc, t_joueur as j where sc.t_joueur_id_joueur = j.id_joueur order by j.pseudo_joueur'));
+	}
+	else if(isset($_GET['liste_com'])) {
+		echo json_encode(GetArray('select c.date_commande, j.pseudo_joueur, p.nom_produit from t_commande as c, t_joueur as j, t_produit as p where j.id_joueur = c.t_joueur_id_joueur and p.id_produit = c.t_produit_id_produit order by j.pseudo_joueur'));
+	}
--- site/app/view/admin/index.php
+++ site/app/view/admin/index.php
@@ -2,7 +2,10 @@
 	<h1>Actions</h1><br/>
 	<div id="liste_j" class="button_wooden">Liste de joueurs</div>
 	<div id="liste_c" class="button_wooden">Liste de cartes</div>
-	<div id="liste_s" class="button_wooden">Liste de serveurs</div>
+	<div id="liste_s" class="button_wooden">Liste de serveurs</div><br/>
+	<div id="liste_p" class="button_wooden">Liste de produits</div>
+	<div id="liste_sc" class="button_wooden">Liste de achats SC</div>
+	<div id="liste_com" class="button_wooden">Liste de commandes</div>
 	<br/><br/>
 	<div id="admin_out">
 		
@@ -10,7 +13,7 @@
 	<script>
 		$("#liste_j").click(function(){
 			$.getJSON('/admin/index&liste_j',function(data){
-				var out = '<table><tr><td>Pseudo</td><td>Mail</td><td>Admin</td></tr>';
+				var out = '<table class="admin_table"><tr><td>Pseudo</td><td>Mail</td><td>Admin</td></tr>';
 				for(var u in data) out += '<tr><td>'+data[u]['pseudo_Joueur']+'</td><td>'+data[u]['mail_Joueur']+'</td><td>'+(data[u]['is_Admin']==1?'yes':'no')+'</td></tr>';
 				out +='</table>';
 				$("#admin_out").html(out);
@@ -19,7 +22,7 @@
 		
 		$("#liste_c").click(function(){
 			$.getJSON('/admin/index&liste_c',function(data){
-				var out = '<table><tr><td>Id</td><td>Nom</td><td>Url</td></tr>';
+				var out = '<table class="admin_table"><tr><td>Id</td><td>Nom</td><td>Url</td></tr>';
 				for(var u in data) out += '<tr><td>'+data[u]['id_Carte']+'</td><td>'+data[u]['nom_Carte']+'</td><td>'+data[u]['url_Carte']+'</td></tr>';
 				out +='</table>';
 				$("#admin_out").html(out);
@@ -27,8 +30,32 @@
 		});
 		$("#liste_s").click(function(){
 			$.getJSON('/admin/index&liste_s',function(data){
-				var out = '<table><tr><td>PID</td><td>Partie</td></tr>';
+				var out = '<table class="admin_table"><tr><td>PID</td><td>Partie</td></tr>';
 				for(var u in data) out += '<tr><td>'+data[u]['pid_Server']+'</td><td>'+data[u]['T_Partie_id_Partie']+'</td></tr>';
+				out +='</table>';
+				$("#admin_out").html(out);
+			});
+		});
+		$("#liste_p").click(function(){
+			$.getJSON('/admin/index&liste_p',function(data){
+				var out = '<table class="admin_table"><tr><td>Nom</td><td>Prix</td></tr>';
+				for(var u in data) out += '<tr><td>'+data[u]['nom_Produit']+'</td><td>'+data[u]['prix_Produit']+'</td></tr>';
+				out +='</table>';
+				$("#admin_out").html(out);
+			});
+		});
+		$("#liste_sc").click(function(){
+			$.getJSON('/admin/index&liste_sc',function(data){
+				var out = '<table class="admin_table"><tr><td>Joueur</td><td>Date</td><td>Montant SC</td><td>Montant Euros</td><td>Code validation</td></tr>';
+				for(var u in data) out += '<tr><td>'+data[u]['pseudo_joueur']+'</td><td>'+data[u]['date_achat_sc']+'</td><td>'+data[u]['montant_sc']+'</td><td>'+data[u]['montant_euros']+'</td><td>'+data[u]['code_validation']+'</td></tr>';
+				out +='</table>';
+				$("#admin_out").html(out);
+			});
+		});
+		$("#liste_com").click(function(){
+			$.getJSON('/admin/index&liste_com',function(data){
+				var out = '<table class="admin_table"><tr><td>Joueur</td><td>Produit</td><td>Date</td></tr>';
+				for(var u in data) out += '<tr><td>'+data[u]['pseudo_joueur']+'</td><td>'+data[u]['date_commande']+'</td><td>'+data[u]['nom_produit']+'</td></tr>';
 				out +='</table>';
 				$("#admin_out").html(out);
 			});
